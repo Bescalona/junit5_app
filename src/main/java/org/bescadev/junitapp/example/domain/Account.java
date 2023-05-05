da783a89a6320942e441a0994baa41ed9140c2dc
Bescalona <bescaubb@gmail.com>
2023-05-05 16:58:15 -0400

    first unit tests

--- src/main/java/org/bescadev/junitapp/example/domain/Account.java
+++ src/main/java/org/bescadev/junitapp/example/domain/Account.java
@@ -1,13 +1,14 @@
 package org.bescadev.junitapp.example.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-
 @Getter
 @Setter
-public class Accouxbont {
+@AllArgsConstructor
+public class Account {
 
     private String person;
     private BigDecimal balance;
--- src/test/java/org/bescadev/junitapp/example/domain/AccountTest.java
+++ src/test/java/org/bescadev/junitapp/example/domain/AccountTest.java
@@ -2,11 +2,30 @@ package org.bescadev.junitapp.example.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
 
     @Test
-    void name() {
+    void testAccountName() {
+        Account account = new Account("Bastian", new BigDecimal("1000.12345"));
+        String expected = "Bastian";
+        String real = account.getPerson();
+        assertEquals(expected, real);
+        assertTrue(real.equals("Bastian"));
+    }
+
+    @Test
+    void testAccountBalance() {
+        Account account = new Account("Bastian", new BigDecimal("1000.12345"));
+        assertEquals(1000.12345, account.getBalance().doubleValue());
+        assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) > 0);
+    }
+
+    @Test
+    void testAccountReference() {
+
     }
 }
